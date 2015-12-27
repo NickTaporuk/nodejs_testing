e543eaa077f81004b97eb12db2fe74ee105e41eb
Nickolay Kiropatkin <nictaporuk@yandex.ru>
2015-12-27 13:04:08 +0200

    init user data

--- etsy.js
+++ etsy.js
@@ -1,4 +1,4 @@
-var user = require('user');
+var user = require('./user');
 var webdriver = require('selenium-webdriver'),
     By = require('selenium-webdriver').By,
     until = require('selenium-webdriver').until;
@@ -6,13 +6,15 @@ var webdriver = require('selenium-webdriver'),
 var driver = new webdriver.Builder()
     .forBrowser('firefox')
     .build();
-var url = 'http://etsy.com'
+var	url = 'http://etsy.com',
+	User = new user();
 etsy(url,driver,By);
+console.log('UserData', User.data().name);
 
-function etsy(url, driver, By) {
+function etsy(url, driver, By, user) {
 	driver.get(url);
 	driver.findElement(By.name('q')).sendKeys('webdriver');
 	driver.findElement(By.name('btnG')).click();
 	driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-	driver.quit();		
+	driver.quit();	
 }
